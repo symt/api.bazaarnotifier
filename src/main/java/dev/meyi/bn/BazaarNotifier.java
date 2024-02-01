83704429469a42b22509424c31d4aa787e9a6e93
symt <28537235+symt@users.noreply.github.com>
2024-02-01 14:00:18 -0600

    Empty Spring Boot Project

---
+++ src/main/java/dev/meyi/bn/BazaarNotifier.java
@@ -0,0 +1,13 @@
+package dev.meyi.bn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazaarNotifier {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazaarNotifier.class, args);
+	}
+
+}
